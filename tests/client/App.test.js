e0844fc05aaa710c8d088f2301e51b5a934814b1
andela-jwarugu <joy.warugu@andela.com>
2016-12-08 12:33:19 +0300

    [Chore]

---
+++ tests/client/App.test.js
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+// import App from '../../client'
+
+describe('<App /> test suite', () => {
+  it('asserts that component did mount is called once', () => {
+    expect(true).to.be.true;
+  })
+})
