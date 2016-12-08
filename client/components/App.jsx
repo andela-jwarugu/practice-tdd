519a7e18fc301a745055fbcbcc4362d2a30e00a2
andela-jwarugu <joy.warugu@andela.com>
2016-12-08 18:47:55 +0300

    [Feature]

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,61 @@
 import React from 'react';
+import request from 'superagent';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      base: '',
+      symbol: '',
+      currency: [],
       rates: {},
       specificRates: {},
-      isConverted: false
+      isConverted: false,
+      amount: null,
+      convertedAmount: null
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.baseChange = this.baseChange.bind(this);
+    this.symbolChange = this.symbolChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount(){
+  handleChange(event) {
+    this.setState({amount: event.target.value});
+  }
 
+  baseChange(event) {
+    this.setState({base: event.target.value})
+  }
+
+  symbolChange(event) {
+    this.setState({symbol: event.target.value})
+  }
+
+  handleClick() {
+    request
+      .get('http://localhost:3000/api/rates/currency?base='+ this.state.base + '&symbols=' + this.state.symbol)
+      .then((res) => {
+        this.setState({rates: res.body.rates, isConverted: true});
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount() {
+    request
+      .get('http://localhost:3000/api/rates')
+      .then((res) => {
+        this.setState({currency: Object.keys(res.body.rates)})
+      }).catch(err => {
+        console.log(err);
+      })
   }
 
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <nav className='navbar navbar-light bg-faded'>
@@ -30,35 +70,35 @@ export default class App extends React.Component {
             <div className='col-md-6 offset-md-3'>
 
               <div className='input-group'  style={{marginTop: '1.2em'}}>
-                <input type='number' className='form-control' placeholder='Enter amount to be converted'></input>
+                <input type='number' className='form-control' onChange={this.handleChange} placeholder='Enter amount to be converted'></input>
               </div> <br/>
 
               <span>
                 <span> From </span>
-                <select className='selectpicker' style={{marginLeft: 5}}>
-                  <option>First</option>
-                  <option>Second</option>
-                  <option>Second</option>
+                <select className='selectpicker' onChange={this.baseChange} style={{marginLeft: 5}}>
+                  {this.state.currency.map((unit) => (
+                    <option key={unit} value={unit}>{unit}</option>
+                  ))}
                 </select>
                 <span style={{marginLeft: 10}}> To </span>
-                <select className='selectpicker' style={{marginLeft: 5}}>
-                  <option>First</option>
-                  <option>Second</option>
-                  <option>Second</option>
+                <select className='selectpicker' onChange={this.symbolChange} style={{marginLeft: 5}}>
+                  {this.state.currency.map((unit) => (
+                    <option key={unit} value={unit}>{unit}</option>
+                  ))}
                 </select>
               </span>
 
               <div style={{marginTop: '1.2em'}}>
-                <button type='button' className='btn btn-primary'> Convert </button>
+                <button type='button' className='btn btn-primary' onClick={this.handleClick}> Convert </button>
               </div> <br/>
 
 
             </div>
 
-            {!this.state.isConverted ?
+            {this.state.isConverted ?
               <div className='col-md-12'>
                 <hr/>
-                <p className='lead'> This amount is equivalent to this amount</p>
+                <p className='lead'> {this.state.base} {this.state.amount} is equivalent to {this.state.symbol} </p>
               </div> : <p></p>
             }
           </div>
