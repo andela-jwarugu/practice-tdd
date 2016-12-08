2526550360538ee1fe50592564f6869f51de31b0
andela-jwarugu <joy.warugu@andela.com>
2016-12-08 12:43:19 +0300

    [Feature]

--- tests/client/App.test.js
+++ tests/client/App.test.js
@@ -1,8 +1,18 @@
 import {expect} from 'chai';
-// import App from '../../client'
+import React from 'react';
+import App from '../../client/components/App.jsx'
+import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
 
 describe('<App /> test suite', () => {
   it('asserts that component did mount is called once', () => {
-    expect(true).to.be.true;
+    sinon.spy(App.prototype, 'componentDidMount');
+    const wrapper = mount(<App />)
+    expect(App.prototype.componentDidMount.calledOnce).to.be.true;
   })
 })
+
+// Test initial state
+// Shallow render and expect some elements
+// Mount with data and check for expected elements on injection of data-toggle
+// Simmulate button click and confirm right action is calles
